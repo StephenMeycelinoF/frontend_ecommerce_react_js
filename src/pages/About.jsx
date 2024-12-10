cce57dc5253304199f2ca62d33e59be6c3426c3f
Stephen Meycelino Fransisco <fstephenmeycelino@gmail.com>
2024-12-11 01:55:51 +0700

    fix: import Components NewsLetterBox

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import Title from "../components/Title";
 import { assets } from "../assets/images/assets";
-import NewsletterBox from "../components/NewsletterBox";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 export default function About() {
   return (
@@ -64,7 +64,7 @@ export default function About() {
         </div>
       </div>
 
-      <NewsletterBox />
+      <NewsLetterBox />
     </div>
   );
 }
